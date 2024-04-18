5d780dcf3f0e093f46250059e43f9c459570e42b
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2024-04-18 22:49:39 +0200

    progress card moved to seperate .js

--- front-end/pdf-app/src/components/RunDownloader.js
+++ front-end/pdf-app/src/components/RunDownloader.js
@@ -2,18 +2,20 @@ import React, { useState, useEffect, useContext } from 'react';
 import api from '../api/api.js';
 import { AuthContext } from '../contexts/auth.js';
 import { useNavigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
+import Toast from 'react-bootstrap/Toast';
 import { Card, Button, Form, InputGroup, FormControl, DropdownButton, Dropdown, Row, Col, ProgressBar } from 'react-bootstrap';
+import DownloadProgress from './DownloadProgress';
 
 const RunDownloader = () => {
     const navigate = useNavigate();
     const { userToken, handleContextLogin, isAdmin, setuserToken } = useContext(AuthContext);
-    const decodedToken = userToken ? jwtDecode(userToken) : null;
 
     const [files, setFiles] = useState([]);
     const [formValues, setFormValues] = useState({ startRow: 0, numRows: 0, selectedFile: "" });
-    const [taskId, setTaskId] = useState(null);
-    const [downloadProgress, setDownloadProgress] = useState(null);
+    const [taskIds, setTaskIds] = useState([]);
+
+    const [showToast, setShowToast] = useState(false);
+    const [toastMessage, setToastMessage] = useState('');
 
     useEffect(() => {
         try {
@@ -53,44 +55,6 @@ const RunDownloader = () => {
         }
     };
 
-    useEffect(() => {
-        let interval = null;
-
-        if (taskId && (!downloadProgress || downloadProgress.status !== 'finished')) {
-            interval = setInterval(() => {
-                api.get(`/download_results/${taskId}`, {
-                    headers: {
-                        'Authorization': `Bearer ${userToken}`,
-                    },
-                })
-                    .then(response => {
-                        const { num_rows, processed_rows, results, status, start_time, running_time, start_row, running_file } = response.data;
-                        const progress = (processed_rows / num_rows) * 100;
-                        setDownloadProgress({
-                            progress,
-                            results,
-                            status,
-                            start_time,
-                            running_time,
-                            start_row,
-                            num_rows,
-                            processed_rows,
-                            running_file
-                        });
-                    })
-                    .catch(error => {
-                        console.error('Error fetching download results:', error);
-                    });
-            }, 1000);
-        }
-
-        return () => {
-            if (interval) {
-                clearInterval(interval);
-            }
-        };
-    }, [taskId, downloadProgress]);
-
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -109,24 +73,25 @@ const RunDownloader = () => {
             });
 
             console.log(response.data);
-            setTaskId(response.data.task_id);
-            setDownloadProgress(null);  // Reset downloadProgress when a new download is started
+            setTaskIds(prevTaskIds => [...prevTaskIds, response.data.task_id]);
         } catch (error) {
-            console.error('Error starting download manager:', error);
+            console.error('Failed to fetch resource:', error);
+            let errorMessage = 'Failed to fetch resource.';
+            if (error.response) {
+                if (error.response.data && error.response.data.detail) {
+                    errorMessage = typeof error.response.data.detail === 'object'
+                        ? JSON.stringify(error.response.data.detail)
+                        : error.response.data.detail;
+                } else if (error.response.status === 404) {
+                    errorMessage = 'Resource not found.';
+                } else if (error.response.status === 401) {
+                    errorMessage = 'Unauthorized.';
+                }
+            }
+            setToastMessage(errorMessage);
+            setShowToast(true);
         }
     };
-
-    const [finalElapsedTime, setFinalElapsedTime] = useState(null);
-
-    useEffect(() => {
-        if (downloadProgress && downloadProgress.status === 'finished') {
-            const elapsedSeconds = Math.floor((new Date() - new Date(downloadProgress.start_time)) / 1000) + downloadProgress.running_time;
-            const hours = Math.floor(elapsedSeconds / 3600);
-            const minutes = Math.floor((elapsedSeconds % 3600) / 60);
-            const seconds = (elapsedSeconds % 60).toFixed(2);
-            setFinalElapsedTime(`${hours} hours, ${minutes} minutes, ${seconds} seconds`);
-        }
-    }, [downloadProgress]);
 
     return (
         <Card>
@@ -157,80 +122,36 @@ const RunDownloader = () => {
                     </div>
                     <div>
                         <Button variant="primary" type="submit" style={{ marginRight: '10px' }}>Start Download</Button>
-                        <Button variant="secondary" onClick={refreshFiles}>Refresh Files</Button>
+                        <Button variant="secondary" onClick={refreshFiles} style={{ marginRight: '10px' }}>Refresh Files</Button>
+                        <Button variant="danger" onClick={() => setTaskIds([])}>Clear Tasks</Button>
                     </div>
-                </Form>
-                <Card style={{ marginTop: '20px' }}>
-                    <Card.Body>
-                        {downloadProgress && formValues.numRows !== 0 && (
-                            <>
-                                <ProgressBar
-                                    now={downloadProgress.progress}
-                                    label={`${downloadProgress.progress.toFixed(2)}%`}
-                                    variant={downloadProgress.status === 'running' ? 'warning' : 'success'}
-                                    style={{
-                                        height: '30px', // Increase the thickness of the progress bar
-                                        fontSize: '18px', // Increase the font size of the label
-                                        fontWeight: 'bold',
-                                        lineHeight: '30px', // Vertically center the label
-                                    }}
+                    <Card style={{ marginTop: '20px' }}>
+                        <Card.Body>
+                            {formValues.numRows !== 0 && taskIds.map(taskId => (
+                                <DownloadProgress
+                                    key={taskId}
+                                    taskId={taskId}
                                 />
-                                <table style={{ marginTop: '20px' }}>
-                                    <tbody>
-                                        <tr>
-                                            <td>Status:</td>
-                                            <td>{downloadProgress.progress === 100 && downloadProgress.status === 'running' ? 'finalizing' : downloadProgress.status}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Running file:</td>
-                                            <td>{downloadProgress.running_file}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Successful:</td>
-                                            <td>{downloadProgress.results.successful}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Already downloaded:</td>
-                                            <td>{downloadProgress.results.already_downloaded}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Failed:</td>
-                                            <td>{downloadProgress.results.failed}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Processed rows:</td>
-                                            <td>{downloadProgress.processed_rows} out of {downloadProgress.num_rows}</td>
-                                        </tr>
-                                        {
-                                            downloadProgress.start_time && (
-                                                <tr>
-                                                    <td>Elapsed time:</td>
-                                                    <td>
-                                                        {downloadProgress.status !== 'finished' ? (
-                                                            (() => {
-                                                                const elapsedSeconds = Math.floor((new Date() - new Date(downloadProgress.start_time)) / 1000) + downloadProgress.running_time;
-                                                                const hours = Math.floor(elapsedSeconds / 3600);
-                                                                const minutes = Math.floor((elapsedSeconds % 3600) / 60);
-                                                                const seconds = (elapsedSeconds % 60).toFixed(2);
-                                                                return `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
-                                                            })()
-                                                        ) : (
-                                                            // Display the final elapsed time when the status is 'finished'
-                                                            finalElapsedTime
-                                                        )}
-                                                    </td>
-                                                </tr>
-                                            )
-                                        }
-                                    </tbody>
-                                </table>
-                            </>
-                        )}
-
-                    </Card.Body>
-                </Card>
-            </Card.Body>
-        </Card>
+                            ))}
+                        </Card.Body>
+                    </Card>
+                </Form>
+                <Toast
+                    style={{
+                        position: 'absolute',
+                        top: '30%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                    }}
+                    show={showToast}
+                    onClose={() => setShowToast(false)}
+                    delay={3000}
+                    autohide
+                >
+                    <Toast.Body>{toastMessage}</Toast.Body>
+                </Toast>
+            </Card.Body >
+        </Card >
     );
 };
 
