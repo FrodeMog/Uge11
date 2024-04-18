7cf6575acee5bfdaf15c2d2dc0c920bbf5d04057
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2024-04-18 15:32:19 +0200

    upload excel to admin dashboard

--- front-end/pdf-app/src/components/UploadExcel.js
+++ front-end/pdf-app/src/components/UploadExcel.js
@@ -3,18 +3,90 @@ import api from '../api/api.js';
 import { AuthContext } from '../contexts/auth.js';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
+import Toast from 'react-bootstrap/Toast';
+import { Checkbox, Card, Button, Form, InputGroup, FormControl, DropdownButton, Dropdown, Row, Col } from 'react-bootstrap';
 
 const UploadExcel = () => {
-    const navigate = useNavigate();
-    const { userToken, handleContextLogin, isAdmin, setuserToken } = useContext(AuthContext);
-    const decodedToken = userToken ? jwtDecode(userToken) : null;
+    const { userToken } = useContext(AuthContext);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [overwrite, setOverwrite] = useState(false);
+
+    const [showToast, setShowToast] = useState(false);
+    const [toastMessage, setToastMessage] = useState('');
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleOverwriteChange = (event) => {
+        setOverwrite(event.target.checked);
+    };
+
+    const handleUpload = async () => {
+        if (!selectedFile) {
+            console.error('No file selected');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('files', selectedFile, selectedFile.name);
+
+        try {
+            await api.post(`/upload_files/pdf-urls/${overwrite}`, formData, {
+                headers: {
+                    'Authorization': `Bearer ${userToken}`,
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            console.log('File uploaded successfully');
+            setToastMessage(`File: ${selectedFile.name} uploaded successfully`);
+            setShowToast(true);
+        }  catch (error) {
+            console.error('Failed to fetch resource:', error);
+            let errorMessage = 'Failed to fetch resource.';
+            if (error.response) {
+                if (error.response.data && error.response.data.detail) {
+                    errorMessage = typeof error.response.data.detail === 'object'
+                        ? JSON.stringify(error.response.data.detail)
+                        : error.response.data.detail;
+                } else if (error.response.status === 404) {
+                    errorMessage = 'Resource not found.';
+                } else if (error.response.status === 401) {
+                    errorMessage = 'Unauthorized.';
+                }
+            }
+            setToastMessage(errorMessage);
+            setShowToast(true);
+        }
+    };
 
     return (
         <div>
-            <h2>UploadExcel</h2>
-            <p>Click the button below to UploadExcel</p>
+            <h2>Upload Excel</h2>
+            <p>Click the button below to upload an Excel file</p>
+            <Form>
+                <Form.Control type="file" id="custom-file" label="Choose file" onChange={handleFileChange} />
+                <Form.Check type="checkbox" label="Overwrite existing file" onChange={handleOverwriteChange} />
+                <Button variant="primary" onClick={handleUpload}>
+                    Upload
+                </Button>
+            </Form>
+            <Toast
+                style={{
+                    position: 'absolute',
+                    top: 100,
+                    right: 20,
+                }}
+                show={showToast}
+                onClose={() => setShowToast(false)}
+                delay={3000}
+                autohide
+            >
+                <Toast.Body>{toastMessage}</Toast.Body>
+            </Toast>
         </div>
-    );
 
+    );
 };
+
 export default UploadExcel;
