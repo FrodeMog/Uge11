1a0df13d2150acc6a1a5124ba702f083a091b025
Frederik Mogensen <frode.moge@gmail.com>
2024-04-24 10:55:38 +0200

    give host ip to frontend api

--- front-end/pdf-app/src/api/api.js
+++ front-end/pdf-app/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const host_ip = process.env.REACT_APP_HOST_IP || 'localhost';
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: `http://${host_ip}:8000`,
 });
 
 export default api;
